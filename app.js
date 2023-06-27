335c2b4d0b10561533ae01f5cc005a811f849157
dzmoney <115180206+dzmoney@users.noreply.github.com>
2023-06-26 21:26:07 -0400

    Initial setup

---
+++ app.js
@@ -0,0 +1,10 @@
+//define gamebord, info & startCells with 9 empty
+//make first go circle and set info display
+
+//create board
+//for each of startCells
+//make cellElement a div with class of square
+//add id to each cell = index
+//add event listener to each cell and call addGo()
+//add cell element to gameBoard
+//call createBoard()
